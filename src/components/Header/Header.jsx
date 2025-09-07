083af1d556d1b90e706d3be61c06135a28441572
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-09-07 04:03:01 +0400

    style: Make the Enagram header responsive. This commit focuses on the responsive design of the Enagram application. It adds CSS media queries and adjusts the header layout to provide a seamless user experience across various devices, from mobile to desktop.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
+import { IoMenu, IoClose } from "react-icons/io5";
 import { Logo } from "../Route";
 import Navigation from "./Navigation/Navigation";
 import { useState } from "react";
@@ -6,18 +7,30 @@ import "./Header.scss";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [menu, setMenu] = useState(false);
 
   const handleHeader = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+
   return (
-    <header className={isOpen ? "close" : ""}>
+    <header className={`${isOpen ? "close" : ""} ${menu ? "show" : ""}`}>
       <div className="icons" onClick={handleHeader}>
         {isOpen ? (
           <RxDoubleArrowRight className="row" />
         ) : (
           <RxDoubleArrowLeft className="row" />
+        )}
+      </div>
+      <div className="resp_menu" onClick={handleMenu}>
+        {menu ? (
+          <IoClose className="menu" />
+        ) : (
+          <IoMenu className="menu" />
         )}
       </div>
       <section className="logo_cont">
